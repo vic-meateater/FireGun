8f9d6402a62c98f4055b16b09a788451a2ba7391
vic-meateater <victor.myasoedov@gmail.com>
2023-08-07 22:54:30 +0500

    refactor input system

---
+++ Assets/_ROOT/Scripts/Input/InputSystem.cs
@@ -0,0 +1,31 @@
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using UnityEngine;
+using Unity.IL2CPP.CompilerServices;
+
+[Il2CppSetOption(Option.NullChecks, false)]
+[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+[Il2CppSetOption(Option.DivideByZeroChecks, false)]
+[CreateAssetMenu(menuName = "ECS/Systems/" + nameof(InputSystem))]
+public sealed class InputSystem : UpdateSystem
+{
+    private Filter _inputFilter;
+    private PlayerInputAsset _playerInput;
+    
+    public override void OnAwake()
+    {
+        _inputFilter = World.Filter.With<InputComponent>();
+        _playerInput = new PlayerInputAsset();
+        _playerInput.Enable();
+    }
+
+    public override void OnUpdate(float deltaTime)
+    {
+        foreach (var e in _inputFilter)
+        {
+            ref var input = ref e.GetComponent<InputComponent>();
+            input.DragInput = _playerInput.PlayerInput.Move.ReadValue<Vector2>();
+            input.IsPlayerTouch = _playerInput.PlayerInput.Touch.ReadValue<float>() > 0;
+        }
+    }
+}
--- Assets/_ROOT/Scripts/Systems/DragWeaponSystem.cs
+++ Assets/_ROOT/Scripts/Systems/DragWeaponSystem.cs
@@ -2,8 +2,7 @@ using Scellecs.Morpeh;
 using Scellecs.Morpeh.Systems;
 using UnityEngine;
 using Unity.IL2CPP.CompilerServices;
-using UnityEngine.InputSystem;
-using TouchPhase = UnityEngine.TouchPhase;
+
 
 [Il2CppSetOption(Option.NullChecks, false)]
 [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
@@ -15,89 +14,34 @@ public sealed class DragWeaponSystem : UpdateSystem
     private Camera _camera;
     private Transform _weaponTransform;
 
+
     public override void OnAwake()
     {
-        _filter = World.Filter.With<WeaponHolderComponent>();
-        foreach (var entity in _filter)
+        _filter = World.Filter.With<WeaponHolderComponent>().With<InputComponent>();
+        foreach (var e in _filter)
         {
-            ref var weaponHolder = ref entity.GetComponent<WeaponHolderComponent>();
-
-            _camera = weaponHolder.Camera;
-            _weaponTransform = weaponHolder.Transform;
+         ref var weaponHolder = ref e.GetComponent<WeaponHolderComponent>(); 
+         _camera = weaponHolder.Camera;
+         _weaponTransform = weaponHolder.Transform;
         }
     }
 
     public override void OnUpdate(float deltaTime)
     {
-
-        //TODO: не работает 
-        if (Touchscreen.current.primaryTouch.press.isPressed || Mouse.current.leftButton.isPressed)
+        foreach (var entity in _filter)
         {
-            Vector2 inputPosition = GetInputPosition();
-            UpdateWeaponPosition(inputPosition, _camera, _weaponTransform);
+            ref var inputComponent = ref entity.GetComponent<InputComponent>();
+
+            Vector2 inputPosition = inputComponent.DragInput;
+            bool isPlayerTouch = inputComponent.IsPlayerTouch;
+
+
+            if (isPlayerTouch)
+            {
+                Vector3 touchPosition = _camera.ScreenToWorldPoint(new Vector3(inputPosition.x, inputPosition.y,
+                    _camera.nearClipPlane + 2.5f));
+                _weaponTransform.position = new Vector3(touchPosition.x, touchPosition.y, touchPosition.z);
+            }
         }
-        
-        
-        // foreach (var entity in _filter)
-        // {
-        //     ref var weaponHolder = ref entity.GetComponent<WeaponHolderComponent>();
-        //     
-        //     var camera = weaponHolder.Camera;
-        //     var weaponTransform = weaponHolder.Transform;
-        //     
-        //     if (Input.touchCount > 0)
-        //     {
-        //         //TODO: починить это
-        //         Touch touch = Input.GetTouch(0);
-        //         if (touch.phase == TouchPhase.Moved)
-        //         {
-        //             // Получаем позицию касания в мировых координатах
-        //             Vector3 touchPosition =
-        //                 camera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y,
-        //                     camera.nearClipPlane + 2.5f));
-        //
-        //             // Преобразуем координаты касания в нужный диапазон: X от -1 до 1, Y от -0.5 до 0.5
-        //             float newX = Mathf.Clamp(touchPosition.x, -0.5f, 0.5f);
-        //             float newY = Mathf.Clamp(touchPosition.y, 1f, 2f);
-        //
-        //             // Устанавливаем новую позицию оружия
-        //             weaponTransform.position = new Vector3(touchPosition.x, touchPosition.y, touchPosition.z);
-        //             //weaponTransform.position = touchPosition;
-        //
-        //         }
-        //     }
-        // }
-        
     }
-        private Vector2 GetInputPosition()
-        {
-            Vector2 inputPosition = Vector2.zero;
-
-            if (Touchscreen.current.primaryTouch.press.isPressed)
-            {
-                // Получаем позицию касания для мобильного устройства
-                inputPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-            }
-            else if (Mouse.current.leftButton.isPressed)
-            {
-                // Получаем позицию курсора для мыши
-                inputPosition = Mouse.current.position.ReadValue();
-            }
-
-            return inputPosition;
-        }
-        
-        private void UpdateWeaponPosition(Vector2 inputPosition, Camera camera, Transform weaponHolderTransform)
-        {
-            // Преобразуем координаты в мировые координаты
-            Vector3 touchPosition = camera.ScreenToWorldPoint(new Vector3(inputPosition.x, inputPosition.y,
-                camera.nearClipPlane + 2.5f));
-
-            // Преобразуем координаты касания в нужный диапазон: X от -1 до 1, Y от -0.5 до 0.5
-            float newX = Mathf.Clamp(touchPosition.x, -0.5f, 0.5f);
-            float newY = Mathf.Clamp(touchPosition.y, -0.5f, 0.5f);
-
-            // Устанавливаем новую позицию оружия
-            weaponHolderTransform.position = new Vector3(newX, newY, touchPosition.z);
-        }
 }
